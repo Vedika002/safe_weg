858a87339258c4fe927988f86d79b22fb893aa0e
Vedika002 <91604900+Vedika002@users.noreply.github.com>
2022-05-29 12:49:16 +0530

    Delete safe_weg-master directory

--- safe_weg-master/android/app/src/main/kotlin/com/dotdevelopingteam/kartenapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dotdevelopingteam.kartenapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- safe_weg-master/lib/setter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_database/firebase_database.dart';
-import 'package:location/location.dart';
-
-class SetD
-{
-  FirebaseDatabase database_realtime = FirebaseDatabase.instance;
-}
